9f7b32744b1c5cfaa6c2dfd535724f69eb77b340
perinze <perinzefaper@foxmail.com>
2023-04-08 00:05:50 +0800

    encode cpp implemented

--- leb128.cpp
+++ leb128.cpp
@@ -2,23 +2,27 @@
 
 std::pair<unsigned, unsigned> encodeLEB128(unsigned x) {
 
-    unsigned high = x >> 7;
-    high &= 0b11111;
+    x &= 0xfff;
 
-    unsigned low = x;
-    low &= 0b1111111;
+    // upper most 6 bits is needed to set MSB
+    unsigned upper6 = x >> 6;
 
-    // high_valid indicate high is neither 0b00000 nor 0b11111
     bool high_valid = 1;
-
-    if (high == 0b00000) {
+    if (upper6 == 0b000000) {
         high_valid = 0;
     }
-    if (high == 0b11111) {
+    if (upper6 == 0b111111) {
         high_valid = 0;
     }
 
-    unsigned result = 0b00000; // if high_valid is 0
+    bool sign = x >> 11;
+    unsigned low = x & 0b1111111;
+    unsigned high = x >> 7;
+    if (sign == 1) {
+        high |= 0b1100000;
+    }
+
+    unsigned result = 0b0000000; // if high_valid is 0 and is positive
     if (high_valid) { // else
         result = high;
     }
@@ -37,6 +41,42 @@ std::pair<unsigned, unsigned> encodeLEB128(unsigned x) {
     return std::make_pair(result, size);
 }
 
-int main() {
+struct testcase {
+    unsigned riscv;
+    unsigned leb128;
+    unsigned leb128_size;
+    testcase(unsigned riscv, unsigned leb128, unsigned leb128_size):
+        riscv(riscv), leb128(leb128), leb128_size(leb128_size) {}
+};
 
+int main() {
+    std::vector<testcase> cases {
+        testcase(0x0184, 0x0384, 2),
+        testcase(0x0fc0, 0x0040, 1),
+        testcase(0x0040, 0x00c0, 2),
+        testcase(0x07ff, 0x0fff, 2),
+        testcase(0x0000, 0x0000, 1),
+        testcase(0x0800, 0x7080, 2),
+        testcase(0x0fff, 0x007f, 1),
+        testcase(0x0001, 0x0001, 1),
+    };
+    for (testcase c : cases) {
+        auto res = encodeLEB128(c.riscv);
+        bool case_name_printed = false;
+        if (res.first != c.leb128) {
+            if (!case_name_printed) {
+                std::cerr << "in case of input 0x" << std::hex << c.riscv << std::endl;
+                case_name_printed = true;
+            }
+            std::cerr << "0x" << c.leb128 << " expected, however 0x" << res.first << " get\n";
+        }
+        if (res.second != c.leb128_size) {
+            if (!case_name_printed) {
+                std::cerr << "in case of input " << std::hex << c.riscv << std::endl;
+                case_name_printed = true;
+            }
+            std::cerr << std::dec << "size " << c.leb128_size << " expected, however " << res.second << " get\n";
+        }
+    }
+    return 0;
 }
