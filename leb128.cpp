64dee27668b0d0dd5ff0ee2babfac748b333b478
perinze <perinzefaper@foxmail.com>
2023-04-08 23:42:28 +0800

    branch done

--- leb128.cpp
+++ leb128.cpp
@@ -126,7 +126,7 @@ size_t translateIType(unsigned char *wasm, const unsigned long long *riscv, unsi
 }
 
 size_t translateRType(unsigned char *wasm, const unsigned long long *riscv, unsigned char opcode) {
-    unsigned long long instr = *riscv;
+    unsigned instr = *riscv;
     instr >>= 7;
     unsigned dst = instr & 0b11111;
     instr >>= 8;
@@ -230,7 +230,7 @@ void generateTargetTable(unsigned *riscv) {
         tmp <<= 5;
         imm |= tmp; // [11:0]
         //std::cerr << fmt::format("imm[11:0] {:#b}\n", imm);
-        instr >>= 1;
+        instr >>= 6;
         tmp = instr & 1;
         bool flag = 1; // forward
         if (tmp == 1) {
@@ -245,6 +245,83 @@ void generateTargetTable(unsigned *riscv) {
         p += 1; // in asm p += 4 since instruction is 4 bytes
         instr = *p;
     }
+}
+
+size_t translateBranch(unsigned char *wasm, const unsigned *riscv, unsigned char opcode) {
+    unsigned instr = *riscv;
+
+    instr >>= 7;
+    int imm = instr & 0b11111; // [4:1|11]
+    instr >>= 8;
+    unsigned tmp = imm & 1;
+    if (tmp == 1) {
+        imm |= 0b100000000000;
+    }
+    imm &= 0xfffffffe; // [11] [4:0]
+
+    unsigned src = instr & 0b11111;
+    instr >>= 5;
+    unsigned tar = instr & 0b11111;
+    instr >>= 5;
+
+    tmp = instr & 0b111111;
+    tmp <<= 5;
+    imm |= tmp; // [11:0]
+    instr >>= 6;
+    tmp = instr & 1;
+    bool flag = 1; // forward
+    if (tmp == 1) {
+        imm |= 0xfffff000;
+        flag = 0; // backward
+    }
+
+    unsigned char *p = wasm;
+
+    if (flag == 1) { // forward
+        *p = 0x02;
+        p += 1;
+    }
+
+    if (src == 0) {
+        // i32.const 0
+        *p = 0x0041; // store halfword
+        p += 2;
+    } else {
+        // get_local tmp
+        *p = 0x20; // store byte
+        p += 1;
+        unsigned tmp = regMap[src];
+        *p = tmp; // store byte
+        p += 1;
+    }
+
+    if (tar == 0) {
+        // i32.const 0
+        *p = 0x0041; // store halfword
+        p += 2;
+    } else {
+        // get_local tmp
+        *p = 0x20; // store byte
+        p += 1;
+        unsigned tmp = regMap[tar];
+        *p = tmp; // store byte
+        p += 1;
+    }
+
+    // opcode
+    *p = opcode; // store byte
+    p += 1;
+
+    // br_if 0
+    *p = 0x000d; // store halfword
+    p += 2;
+
+    if (flag == 0) { // forward
+        *p = 0x0b;
+        p += 1;
+    }
+
+    return (unsigned)(p - wasm);
 }
 
 
